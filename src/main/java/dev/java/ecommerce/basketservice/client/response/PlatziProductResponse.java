558c4680ea2a506746bc4327a1034f1fccf39ad1
Kamille Rabelo <kamillersalgado@gmail.com>
2025-10-02 17:28:22 -0300

    feat: integrate with `Platzi Store API`

---
+++ src/main/java/dev/java/ecommerce/basketservice/client/response/PlatziProductResponse.java
@@ -0,0 +1,10 @@
+package dev.java.ecommerce.basketservice.client.response;
+
+import java.math.BigDecimal;
+
+public record PlatziProductResponse(
+        Long id,
+        String title,
+        BigDecimal price
+) {
+}
